9ba719059d42fb60a1b8833d2a2e00b8b45cbf04
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2022-11-06 16:40:06 +0700

    add x-access-token

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var usersRouter = require('./components/users');
 var app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
   next();
 });
 // view engine setup
