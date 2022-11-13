c984dad362f1585ff44d015e675d34545a065f35
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2022-11-13 20:44:12 +0700

    remove creadentials

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var usersRouter = require('./components/users');
 var cors = require('cors');
 var app = express();
 app.use(cors({
-  origin: 'https://registration-web-app.netlify.app',
+  origin: '*',
   allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
 }));
 var passport = require('passport');
