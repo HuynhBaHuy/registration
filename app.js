81289795b8aab38126d00d157688f8425883b4e4
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2022-11-13 19:01:55 +0700

    enable cors

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ require('dotenv').config()
 var indexRouter = require('./routes/index');
 var usersRouter = require('./components/users');
 var app = express();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 var passport = require('passport');
-var {applyPassportStrategy} = require('./middlewares/passport');
+var { applyPassportStrategy } = require('./middlewares/passport');
 applyPassportStrategy(passport);
 
 // view engine setup
@@ -25,12 +30,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
